8810a6346ede19b303ad776523e3ccd99d81c59a
David <dsrose3626@eagle.fgcu.edu>
2019-09-20 11:48:44 -0400

    Refactor user interface to meed changed requirements. Added css file. Performed a QC.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,22 +1,77 @@
 package sample;
 
-import javafx.event.Event;
+import java.sql.Statement;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-
 
 public class Controller {
 
-    @FXML
-    private Button productionLineButton;
-    @FXML
-    private Button recordProductionButton;
-    @FXML
-    private Button productionLogButton;
+  @FXML
+  public TabPane window;
+  @FXML
+  private TextField addProductTxt;
+  @FXML
+  private TextField manufactureProd;
+  @FXML
+  private ChoiceBox<String> typeBox;
+  @FXML
+  private Button addProductBtn;
+  @FXML
+  private TableView productTable;//T
+  @FXML
+  private ListView produceList;//T
+  @FXML
+  private ComboBox<Integer> produceCbo;
+  @FXML
+  private Button produceBtn;
+  @FXML
+  private TextArea productLogTxt;
 
-    @FXML
-    protected void doAction(MouseEvent event) {
-        System.out.println("Button Pressed");
+  /**
+   * Initializes the values in the produce tab combobox
+   */
+  @FXML
+  protected void initialize() {
+    for (int i = 1; i < 11; i++) {
+      produceCbo.getItems().add(i);
     }
+    produceCbo.getSelectionModel().selectFirst();
+    produceCbo.setEditable(true);
+  }
+
+  /**
+   * Adds a product to the product line table in the H2 database
+   * @param event a passed event
+   */
+  @FXML
+  protected void addProductFromProductLine(MouseEvent event) {
+    //this query inserts an item called 'nail' and its attributes into the Product table
+    String queryOne = "Insert Into product(name,type,manufacturer)"
+        + "Values('Nail','Construction','Home Depot');";
+    try {
+      Statement st = new Main().conn.createStatement();
+      st.executeUpdate(queryOne);
+      System.out.println("Product added.");
+      st.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * placeholder event
+   * @param e placeholder
+   */
+  @FXML
+  protected void doAction(MouseEvent e) {
+    System.out.println("Button Pressed");
+  }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,49 +1,46 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-
+/**
+ * @author drose
+ */
 public class Main extends Application {
 
-    static final String JDBC_DRIVER = "org.h2.Driver";
-    static final String DB_URL = "jdbc:h2:../ProductionLine/res/h2";
+  private static final String JDBC_DRIVER = "org.h2.Driver";
+  private static final String DB_URL = "jdbc:h2:./res/ProductionLineDB";
+  public Connection conn;
 
-    Connection conn = null;
-    Statement st = null;
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        String user = null;
-        String pword = null;
-
-        try{
-            Class.forName(JDBC_DRIVER);     //register JDBC driver
-            conn = DriverManager.getConnection(DB_URL);
-            st = conn.createStatement();
-
-            String quireOne = "CREATE TABLE Product;";
-            String quireTwo = "DROP TABLE Product;";
-            st.execute(quireOne);
-            st.execute(quireTwo);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Production Line");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    try {
+      //register JDBC driver
+      Class.forName(JDBC_DRIVER);
+      //creates a connection for every instance of Main
+      conn = DriverManager.getConnection(DB_URL);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    //loads the fxml file, then the css stylesheet
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    Scene scene = new Scene(root, 600, 400);
+    scene.getStylesheets().add(
+        Main.class.getResource("\\..\\ProductionLine.css").toExternalForm()
+    );
+    //opens the application
+    primaryStage.setTitle("Production Line");
+    primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
+    primaryStage.show();
+  }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
